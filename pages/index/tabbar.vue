2bb958576033a75d792f25094a92547c95932e03
pkkwilliam <pkkwilliam@me.com>
2022-02-13 13:51:04 +0800

    good build

--- pages/index/tabbar.vue
+++ pages/index/tabbar.vue
@@ -1,12 +1,5 @@
 <template>
   <view>
-    <u--image
-      :showLoading="true"
-      src="https://cdn.uviewui.com/uview/album/1.jpg"
-      width="80px"
-      height="80px"
-      @click="click"
-    />
     <u-tabbar
       :value="currentIndex"
       @change="change1"
@@ -14,28 +7,11 @@
       :placeholder="true"
       :safeAreaInsetBottom="true"
     >
-      <u-tabbar-item text="首页" icon="home" @click="click1"></u-tabbar-item>
-      <u-tabbar-item text="放映厅" icon="photo" @click="click1"></u-tabbar-item>
-      <u-tabbar-item
-        text="直播"
-        icon="play-right"
-        @click="click1"
-      ></u-tabbar-item>
-      <u-tabbar-item text="首页">
-        <image
-          class="u-page__item__slot-icon"
-          slot="active-icon"
-          src="https://cdn.uviewui.com/uview/common/bell-selected.png"
-        ></image>
-        <image
-          class="u-page__item__slot-icon"
-          slot="inactive-icon"
-          src="https://cdn.uviewui.com/uview/common/bell.png"
-        ></image>
-      </u-tabbar-item>
-      <u-tabbar-item text="我的" icon="account" @click="click1"></u-tabbar-item>
+      <u-tabbar-item text="首页" icon="home" @click="onClickItem(0)" />
+      <u-tabbar-item text="放映厅" icon="photo" @click="onClickItem(0)" />
+      <u-tabbar-item text="直播" icon="play-right" @click="onClickItem(0)" />
+      <u-tabbar-item text="我的" icon="account" @click="onClickItem(0)" />
     </u-tabbar>
-    <!-- js -->
   </view>
 </template>
 
